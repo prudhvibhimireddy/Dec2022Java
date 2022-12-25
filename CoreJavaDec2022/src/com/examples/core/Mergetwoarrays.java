51a477866413bd7434be5d53a069a38e85fe2028
prudhvibhimireddy <prudhvikrishnabhimireddy@gmail.com>
2022-12-25 11:27:53 -0600

    saradha

---
+++ CoreJavaDec2022/src/com/examples/core/Mergetwoarrays.java
@@ -0,0 +1,23 @@
+package com.examples.core;
+
+public class Mergetwoarrays {
+    public static void main(String[] args) {
+        int[] a = {1,3,5};
+        int[] b = {2,4,7,9};
+
+        int[] c = new int[a.length+b.length];
+        for(int i=0;i<c.length;i++){
+            if(i<a.length){
+                c[i] = a[i];
+            }
+            else if(i>=a.length){
+                c[i] = b[i-a.length];
+            }
+        }
+        System.out.print("Arrays after merging : ");
+
+        for(int i=0;i<c.length;i++){
+            System.out.print(c[i]+" ");
+        }
+    }
+}
